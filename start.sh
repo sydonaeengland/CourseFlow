e546655a5b67356d00f3bd83d0c74323850b4f3c
sydonaeengland <sydonaeengland@gmail.com>
2026-05-16 14:02:23 -0500

    Disable SSL for MySQL connection in startup script

--- start.sh
+++ start.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+MYSQL_CMD="mysql -h $DB_HOST -P ${DB_PORT:-3306} -u $DB_USER -p$DB_PASSWORD $DB_NAME --ssl-mode=DISABLED"
+
 echo "Starting gunicorn..."
 gunicorn --bind "0.0.0.0:${PORT:-5000}" --workers 4 app:app &
 GUNICORN_PID=$!
 
 echo "DB_HOST=$DB_HOST DB_PORT=$DB_PORT DB_USER=$DB_USER DB_NAME=$DB_NAME"
 echo "Waiting for MySQL..."
-until mysql -h "$DB_HOST" -P "${DB_PORT:-3306}" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" -e "SELECT 1" 2>&1; do
+until $MYSQL_CMD -e "SELECT 1" 2>&1; do
   echo "MySQL not ready, retrying..."
   sleep 3
 done
 
 echo "Running schema..."
-mysql -h "$DB_HOST" -P "${DB_PORT:-3306}" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" < /app/create_db.sql
+$MYSQL_CMD < /app/create_db.sql
 
-COUNT=$(mysql -h "$DB_HOST" -P "${DB_PORT:-3306}" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" -sN -e "SELECT COUNT(*) FROM users;")
+COUNT=$($MYSQL_CMD -sN -e "SELECT COUNT(*) FROM users;")
 if [ "$COUNT" -eq "0" ]; then
   echo "Populating data..."
-  mysql -h "$DB_HOST" -P "${DB_PORT:-3306}" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" < /app/populate_db.sql
+  $MYSQL_CMD < /app/populate_db.sql
   echo "Done."
 fi
 
